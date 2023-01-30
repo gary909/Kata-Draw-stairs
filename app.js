8558f148473bc1407998f6917cfd6f1bfb25d92d
gary909 <garywhite61@hotmail.com>
2023-01-30 12:41:58 +0100

    code

---
+++ app.js
@@ -0,0 +1,7 @@
+function drawStairs(n) {
+    // your code here
+}
+
+console.log(drawStairs(1)); // "I"
+console.log(drawStairs(3)); // "I\n I\n  I"
+console.log(drawStairs(5)); // "I\n I\n  I\n   I\n    I"
